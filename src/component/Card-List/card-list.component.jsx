217136fad6c6aa9991942b6b2c8c0bb3b6e40906
Prashant <jainprashant781@gmail.com>
2021-06-03 20:13:07 +0530

    Adding my first project on github

---
+++ src/component/Card-List/card-list.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './card-list.styles.css';
+import {Card} from '../Card/card.component';
+
+export const CardList = props =>{
+    return (
+        <div className='card-list'>{props.monsters.map(monsters =>(
+            <Card key={monsters.id} monster={monsters}/>))}
+        </div>
+    )}
